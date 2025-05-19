603bdeb83475af179b3c0bb3308d5ec5578ae4f1
Tuncay Basak <tbasak@k0r1p12.42mulhouse.fr>
2025-05-19 18:21:15 +0200

    Generic array operation centralized using arr_void types.

--- util/apis/array/void/marr_void.h
+++ util/apis/array/void/marr_void.h
@@ -6,7 +6,7 @@
 /*   By: tbasak <tbasak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 12:33:12 by tbasak            #+#    #+#             */
-/*   Updated: 2025/05/19 13:41:50 by tbasak           ###   ########.fr       */
+/*   Updated: 2025/05/19 18:15:09 by tbasak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,28 @@ build_marr_void(void *buff, t_size len, t_size item_size);
 
 t_marr_void USE
 marr_void_marr(t_marr_void marr, t_isize offs, t_size len);
+
+typedef struct s_darr_void
+{
+	void	**buff;
+	t_size	*len;
+	t_size	*cap;
+	t_size	item_count;
+}			t_darr_void;
+
+t_darr_void USE
+darr_str_void(t_darr_str *self)
+{
+	return ((t_darr_void){(void **)&self->buff, &self->len, &self->cap, sizeof(t_str)});
+}
+
+void
+darr_str_push_vstr(t_darr_str *self, t_vstr vstr)
+{
+	t_str	str;
+
+	str = str_from_vstr(vstr);	
+	darr_void_push_own(darr_str_void(self), &str, drop_str);
+}
 
 #endif
